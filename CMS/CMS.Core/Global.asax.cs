de6c7b118bcd3a5a7ef9fd964090b080d072d215
tearth <tearthtearthtearth@gmail.com>
2017-03-03 18:57:10 +0100

    Configure NLog

--- CMS/CMS.Core/Global.asax.cs
+++ CMS/CMS.Core/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NLog;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -16,6 +17,15 @@ namespace CMS.Core
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        void Application_Error(object sender, EventArgs e)
+        {
+            Exception exception = Server.GetLastError();
+            if (exception != null)
+            {
+                LogManager.GetCurrentClassLogger().Log(LogLevel.Error, exception);
+            }
         }
     }
 }
