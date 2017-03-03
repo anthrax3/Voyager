11390ee8e89dd3004690656d25fb2b68497824dd
tearth <tearthtearthtearth@gmail.com>
2017-03-03 21:01:34 +0100

    Add database context

---
+++ CMS/CMS.Core/DAL/DatabaseContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CMS.Core.DAL
+{
+    public class DatabaseContext : DbContext, IDatabaseContext
+    {
+        public DatabaseContext()
+        {
+            
+        }
+
+        public int Save()
+        {
+            return base.SaveChanges();
+        }
+
+        public DbSet<TEntity> Get<TEntity>() where TEntity : class
+        {
+            return base.Set<TEntity>();
+        }
+    }
+}
---
+++ CMS/CMS.Core/DAL/IDatabaseContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.Core.DAL
+{
+    public interface IDatabaseContext
+    {
+        DbSet<TEntity> Get<TEntity>() where TEntity : class;
+        int Save();
+    }
+}
