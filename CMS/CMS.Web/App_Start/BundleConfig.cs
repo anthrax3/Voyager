ce686abd0e66427a90a345bdf2cd6a3b4507e43e
tearth <tearthtearthtearth@gmail.com>
2017-03-11 19:55:31 +0100

    Install bootstrap

--- CMS/CMS.Web/App_Start/BundleConfig.cs
+++ CMS/CMS.Web/App_Start/BundleConfig.cs
@@ -13,6 +13,13 @@ namespace CMS.Web
 
             bundles.Add(new ScriptBundle("~/bundles/jquery-validate").Include(
                         "~/Scripts/jquery.validate*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                        "~/Scripts/bootstrap.js"));
+
+            bundles.Add(new StyleBundle("~/styles/bootstrap").Include(
+                        "~/Content/bootstrap.css",
+                        "~/Content/bootstrap-theme.css"));
         }
     }
 }
