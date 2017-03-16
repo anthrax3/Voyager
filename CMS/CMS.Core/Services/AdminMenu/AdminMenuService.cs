a406b89c2513a33c1c4258427fe5389628b568ed
tearth <tearthtearthtearth@gmail.com>
2017-03-16 13:32:15 +0100

    Add admin menu service

---
+++ CMS/CMS.Core/Services/AdminMenu/AdminMenuService.cs
@@ -0,0 +1,24 @@
+﻿using CMS.Core.DB;
+using CMS.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.Core.Services.AdminMenu
+{
+    internal class AdminMenuService
+    {
+        IDatabaseContext db = null;
+        public AdminMenuService(IDatabaseContext db)
+        {
+            this.db = db;
+        }
+
+        public List<AdminMenuItem> GetMenuItems()
+        {
+            return db.Set<AdminMenuItem>().ToList();
+        }
+    }
+}
---
+++ CMS/CMS.Core/Services/AdminMenu/IAdminMenuService.cs
@@ -0,0 +1,14 @@
+﻿using CMS.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.Core.Services.AdminMenu
+{
+    public interface IAdminMenuService
+    {
+        List<AdminMenuItem> GetMenuItems();
+    }
+}
