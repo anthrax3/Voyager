fe78f7ec5ea1378ff0e911def56188c74d56ed65
tearth <tearthtearthtearth@gmail.com>
2017-03-18 16:54:47 +0100

    Add basic structure to categories component

---
+++ CMS/Components/ComCategories/Services/CategoriesService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComCategories.Services
+{
+    internal class CategoriesService : ICategoriesService
+    {
+    }
+}
---
+++ CMS/Components/ComCategories/Setup.cs
@@ -0,0 +1,30 @@
+﻿using CMS.Core.Components;
+using ComCategories.Models;
+using ComCategories.Services;
+using Microsoft.Practices.Unity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComCategories
+{
+    public class Setup : IComponent
+    {
+        public string Name { get; set; } = "ComCategories";
+
+        public bool SetupDatabase(DbModelBuilder builder)
+        {
+            builder.Entity<CategoryModel>().ToTable("Categories");
+
+            return true;
+        }
+
+        public void SetupUnity(IUnityContainer container)
+        {
+            container.RegisterType<ICategoriesService, CategoriesService>();
+        }
+    }
+}
